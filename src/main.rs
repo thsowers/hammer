2f53de26c0177df1dcfc912a17c6195aaa8cbdac
thsowers <tyler@startrakstudios.com>
2019-02-07 00:04:12 -0500

    Remove counters

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,15 @@ fn main() {
     hammer(0);
 }
 
-fn hammer(num: i32) {
-    let counter = Arc::new(Mutex::new(num));
+fn hammer(num: usize) {
     let mut handles = vec![];
 
     for _ in 0..rayon::current_num_threads() {
-        let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
             reqwest::get("https://api.poempath.com/randomPoem")
                 .unwrap()
                 .text()
                 .unwrap();
-
-            let mut num = counter.lock().unwrap();
-            *num += 1;
         });
         handles.push(handle);
     }
@@ -28,7 +23,6 @@ fn hammer(num: i32) {
         handle.join().unwrap();
     }
 
-    println!("Requests Sent: {}", *counter.lock().unwrap());
-
-    hammer(*counter.lock().unwrap());
+    println!("Requests Sent: {}", num);
+    hammer(rayon::current_num_threads() + num);
 }
